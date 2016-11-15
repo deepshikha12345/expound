cf19ebf170b8817d00f3da73380794f35036c823
deepshikha12345 <deepshikhaparmar89@gmail.com>
2016-11-15 15:09:29 +0530

    Add files via upload

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_imudxc',
+  ),
+  'crypt' => 
+  array (
+    'key' => 'df189983f709eb4792b42d83666d5758',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magento2',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 0,
+    'layout' => 0,
+    'block_html' => 0,
+    'collections' => 0,
+    'reflection' => 0,
+    'db_ddl' => 0,
+    'eav' => 0,
+    'customer_notification' => 0,
+    'full_page' => 0,
+    'config_integration' => 0,
+    'config_integration_api' => 0,
+    'translate' => 0,
+    'config_webservice' => 0,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 27 Aug 2016 09:03:46 +0000',
+  ),
+);
